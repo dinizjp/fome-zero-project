dddb107dcddf60b00a28c9408236573addcc192a
João Pedro Diniz <joopedrodiniz@gmail.com>
2023-05-17 19:21:39 -0300

    Update 3_🌍_Métricas_Países.py

--- pages/3_🌍_Métricas_Países.py
+++ pages/3_🌍_Métricas_Países.py
@@ -2,7 +2,7 @@ import pandas as pd
 import streamlit as st
 import plotly.express as px
 
-st.set_page_config( page_title = 'Restaurantes', layout = 'wide')
+st.set_page_config( page_title = 'Restaurantes', layout = 'wide',theme="light")
 
 
                         #Funções 
